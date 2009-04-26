06b0b7daf5e85ccb8d2a1ab8c8ad722b424c9137
Szymon Nowak <szimek@gmail.com>
2009-04-26 18:18:03 +0200

    Minor changes

--- lib/twitter_clone_client.rb
+++ lib/twitter_clone_client.rb
@@ -7,8 +7,8 @@ require 'activeresource'
 module TwitterClone
   
   class Status < ActiveResource::Base
-    self.site = "http://localhost:3000/"
-    Status.prefix = "/users/:user_id/"
+    self.site   = "http://localhost:3000/"
+    self.prefix = "/users/:user_id/"
 
     def to_s
       "#{user.login}:\t #{body} at #{created_at}"
@@ -22,7 +22,6 @@ module TwitterClone
     def initialize
       @opts = Trollop::options do
         banner "Twitter clone RESTful client"
-        version "0.0.1"
         stop_on SUB_COMMANDS
       end
 
@@ -66,14 +65,22 @@ module TwitterClone
 
     def create
       setup_credentials
-      Status.create(:body => @cmd_opts[:text], :user_id => @cmd_opts[:login])
-      puts "Status created successfully"
+      begin
+        Status.create(:body => @cmd_opts[:text], :user_id => @cmd_opts[:login])
+        puts "Status created successfully"
+      rescue ActiveResource::UnauthorizedAccess => e
+        puts e.message and exit
+      end
     end
 
     def delete
       setup_credentials
-      Status.delete(@cmd_opts[:id], :user_id => @cmd_opts[:login])
-      puts "Status destroy successfully"
+      begin
+        Status.delete(@cmd_opts[:id], :user_id => @cmd_opts[:login])
+        puts "Status destroy successfully"
+      rescue ActiveResource::UnauthorizedAccess => e
+        puts e.message and exit
+      end
     end
 
     private
